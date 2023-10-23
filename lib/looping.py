b23b96d4408cd1905481c8850ee6246618a42d69
Kat Tannehill <katetannehill@gmail.com>
2023-10-23 17:56:58 -0600

    Complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    squared_ints = [num * num for num in int_list]
+    return squared_ints
     pass
 
 def fizzbuzz():
-    # code goes here!
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 != 0:
+            print("Fizz")
+        elif i % 5 == 0 and i % 3 != 0:
+            print("Buzz")
+        elif i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
+        i += 1
     pass
